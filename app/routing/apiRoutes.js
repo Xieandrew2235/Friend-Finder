b4256e8fec273c6b7ee316929792408d82ed5f4c
Andrew <xie.andrew2235@gmail.com>
2019-04-01 22:54:35 -0700

    worked on api routes, and added the post route, which includes two for loops that test for all available matches and possibilities. i have pseudocoded what I need to complete my code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,18 +1,35 @@
 // Require friends.js, which contains the array of random people we created
 var friends = require("../data/friends.js");
 
-app.get("/api/friends.js", function(req, res) {
+module.exports = function (app) {
+  app.get("/api/friends.js", function (req, res) {
     res.json(friends);
   });
 
-  app.post("/api/friends.js", function(req, res) {
+  app.post("/api/friends.js", function (req, res) {
     // Object that will hold the best match for the user. Starts empty because it should be constantly updated with iterations of the for-loop
     var friendMatch = {
-        name: "",
-        photo: "",
-      };
-// For-loop that will run through the "friends" created
-      for (var i = 0; i < friends.length; i++) {
-        var currentFriend = friends[i];
-        console.log(currentFriend.name);
+      name: "",
+      photo: "",
+      difference: "",
+    };
+    // Variable which will calculate difference between the user score and the score of other people in the database
+      var totalDifference;
+
+    // For-loop that will run through the "friends" possibilities
+    for (var i = 0; i < friends.length; i++) {
+      var currentFriend = friends[i];
+      totalDifference = 0;
+      console.log(currentFriend.name);
+
+    // Second for-loop that runs through all the scores of each person in our database
+       for (var j = 0; j < currentFriend.scores.length; j++) {
+        var currentFriendScore = currentFriend.scores[j];
+        var currentUserScore = userScores[j];
+    }
   }
+  // Calculate difference between scores above, which will = totalDifference
+
+  // Match total difference up against friendDifference
+  
+  // Return best match to user
